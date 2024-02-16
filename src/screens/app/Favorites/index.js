6f09bed9576f60bfc7014f141bd40b5345694019
Steven Mirontšuk <steven.mirontsuk1@gmail.com>
2024-02-16 14:32:07 +0200

    Favorite Screen Finished

--- src/screens/app/Favorites/index.js
+++ src/screens/app/Favorites/index.js
@@ -1,16 +1,30 @@
 import React from "react";
-import {View, Text } from "react-native";
+import { View, FlatList, Text } from "react-native"; // Import Text component
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { styles } from "./styles";
+import { products } from "../../../data/products";
+import FavoriteItem from "../../../components/FavoriteItem";
+import Header from "../../../components/Header";
 
 const Favorites = () => {
+    const renderItem = ({ item }) => {
+        return (
+            <FavoriteItem {...item} />
+        );
+    };
+
     return (
         <SafeAreaView>
-            <View>
-                <Text>Favorites</Text>
+            <View style={styles.container}>
+                <Header title="Favorites" />
+                <FlatList
+                    data={products}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => String(item.id)}
+                />
             </View>
         </SafeAreaView>
-    )
-}
+    );
+};
 
-export default Favorites
+export default Favorites;
